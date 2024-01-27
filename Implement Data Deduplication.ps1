c5a9acda2012ff70e3c9dde076bd441f8459be6f
Ali Hojaji <140146049+AliHojaji@users.noreply.github.com>
2024-01-27 12:06:12 +0330

    Add files via upload

---
+++ Implement Data Deduplication.ps1
@@ -0,0 +1,36 @@
+﻿#--- Author : Ali Hojaji ---#
+
+#--*--------------------------------*--#
+#---> Implement Data Deduplication <---#
+#--*--------------------------------*--#
+
+#--> install data deduplication
+Install-WindowsFeature -Name FS-Data-Deduplication
+
+#--> dedup evaluation tool
+ddpeval U:
+ddpeval I:
+ddpeval V:
+
+#--> enable data deduplication 
+Enable-DedupVolume -Volume V: -UsageType HyperV
+
+#--> view and set volume-wide settings
+Get-DedupVolume -Volume V: | Format-List *
+Get-DedupVolume -Volume V: -MinimumFileAge 0
+
+#--> manually run optimization job
+Start-DedupJob -Type Optimization -Volume V: -Priority High -Memory 100 -Cores 100
+
+#--> monitor running jobs
+Get-DedupJob
+
+#--> view overall status
+Get-DedupStatus | Format-List *
+
+#--> view and set schedule
+Get-DedupSchedule
+Set-DedupSchedule -Name ThroughputOptimization -Enabled $false
+
+#--> disable deduplication
+start-DedupJob -Type Unoptimization -Volume V:
